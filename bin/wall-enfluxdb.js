384ef19877dcfb5a74b3ca28cf573c7821115c2a
lotco <kgsin7@gmail.com>
2023-04-07 15:06:50 +0900

    find vanilla  requeset :(

--- bin/wall-enfluxdb.js
+++ bin/wall-enfluxdb.js
@@ -1,13 +1,80 @@
+import url from 'url'
+// import fetch from 'fetch'
+// import request from 'request    '
+
+
 class Wallenfluxdb {
 	_token
-	constructor(token) {
-		this._token=token
+	_influxUrl
+	_data
+	_timeUnit
+	_fetchOptions
+
+	constructor(token, url, timeUnit) { 
+		this._token 		= token
+		this._data 			= []
+		this._influxUrl 	= url
+		this._timeUnit 		= timeUnit
+
+		this._fetchOptions = {
+		    headers: {
+	            Authorization   : `token ${this._token}`,
+	            'Content-Type'    : "text/plain; charset=utf-8",
+    	        'Accept'          : "application/json"
+		    },
+		    
+		    method: 'POST'
+		}
 	}
 
 	get token(){
 		return this._token
 	}
 
+
+	writeData(measurement, tag, field, time){
+	
+		const makeData = (object)=>{
+			let tempString
+			let isFirst = true
+			for (let key of Object.keys(object)) {
+				tag = isFirst 
+					? `${key}=${object.key}`
+					: `${tag},${key}=${tag.key}`			
+			}
+
+			return tempString
+		}
+
+		const tagString = makeData(tag)
+		const fieldString = makeData(field)
+
+		this._data.push = `${measurement},${tagString} ${fieldString} ${time}`
+	}
+	
+	commitData(){
+		let dataString = ""
+
+	    for (const inData of this._data){
+        	dataString = `${dataString}
+				${inData}`
+    	}
+
+		this._fetchOptions['data'] = dataString
+
+
+		console.log(url.parse('https://google.com'))
+
+		// fetch(_influxUrl, _fetchOptions)
+		// .then((data) => {
+		// 	console.log(data)
+		// 	return data.json()
+		// })
+		// .then((res) => {
+		// 	console.log(res)
+		// 	this._data = []
+		// })
+	}
 	test(){
 		console.log('eeeeeva ')
 	}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,12 @@
 import Wallenfluxdb from './../bin/wall-enfluxdb.js'
 
-const WallE = new Wallenfluxdb("tokenccc")
+const WallE = new Wallenfluxdb(process.env.INFLUX_TOKEN, process.env.INFLUX_URL, "ms")
+
+WallE.writeData("walle", {tag:"walle"},{field:10}, new Date().getTime)
+WallE.writeData("eva", {tag:"eva"},{field:20}, new Date().getTime)
+WallE.commitData()
+
+
 
 console.log(WallE.token)
 WallE.test()
