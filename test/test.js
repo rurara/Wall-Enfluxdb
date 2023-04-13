899c3ac2765282747e9e81e010a6d85a9a7fb8f8
lotco <kgsin7@gmail.com>
2023-04-14 02:42:27 +0900

    unit test  추가

--- test/test.js
+++ test/test.js
@@ -1,28 +1,20 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-const Wallenfluxdb = require('../lib/Wall-Enfluxdb.js')
+import dotenv from "dotenv";
+dotenv.config();
+import Wallenfluxdb from '../lib/Wall-Enfluxdb.js'
 
-
-
-// const WallE = new Wallenfluxdb(
-// 	process.env.INFLUX_TOKEN,
-// 	process.env.INFLUX_URL,
-// 	process.env.INFLUX_ORG,
-// 	process.env.INFLUX_BUCKET,
-// 	"ms")
-
+const WallE = new Wallenfluxdb(
+	process.env.INFLUX_TOKEN,
+	process.env.INFLUX_URL,
+	process.env.INFLUX_ORG,
+	process.env.INFLUX_BUCKET,
+	"ms")
 
 test("write data", () => {
-	expect(1).toBe(1);
+	WallE.writeData("walle", {tag:"walle", tag2:"yaho"},{field:140}, new Date().getTime())
+	WallE.commitData()
+	.then((response)=>{
+		expect(response.size).toBe(0);
+	}).catch((error)=>{
+		expect(error).toBe(error)
+	})
 });
-
-
-// test("write data", () => {
-// 	WallE.writeData("walle", {tag:"walle", tag2:"yaho"},{field:140}, new Date().getTime())
-// 	WallE.commitData()
-// 	.then((response)=>{
-// 		expect(response.size).toBe(0);
-// 	}).catch((error)=>{
-// 		expect(error).toThrow(`check error [${error}]`);
-// 	})
-// });
